188a9d6bced711053a32717e019ff76e59ce3722
Abhishek pandey <abhie9336@gmail.com>
2023-04-13 02:47:14 +0530

    modified some code

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.static("public"));
 const port = process.env.PORT || 3000;
 
 //configure body-parser middleware to handle json data
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
