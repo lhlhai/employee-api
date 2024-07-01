fbeda98ded9868b26f3aff91a3058cce8b538444
lhlhai <lhlhai.spkt@gmail.com>
2024-07-01 20:49:57 +1000

    Update connection.js

--- connection.js
+++ connection.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
 	connectionLimit : 100,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-	port:  3306,
+	port:  process.env.DB_HOST,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
