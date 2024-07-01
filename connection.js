cb4ada78ef729193fba05d37648ca259b6eaf01a
lhlhai <lhlhai.spkt@gmail.com>
2024-07-01 15:42:33 +0700

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
 const mysql = require('mysql');
 
 // set up for MySql connection
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '12345678',
-    database: 'employedb'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 
